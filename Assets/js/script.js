9267c7502d285df21b07c230461907e71e4373fe
Lissette Vega <lissettevega@Lissettes-MacBook-Pro.local>
2021-09-24 15:02:57 -0400

    add date, save button, and local storage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,1 +1,47 @@
+var date = document.querySelector("#currentDay")
+var block = document.querySelector("textarea")
+var saveBtn= document.querySelector(".saveBtn")
+
+// Current date displayed on top
+var day = moment ().format('dddd, MMMM Do');
+date.textContent= (day)
+
+// color codes
+var colorTime = function(){
+    var time = new Date().getHours();
+
+if(time<10){
+    $(block).addClass ('.present');
+}
+
+
+    // if(day === (".hour"){
+    //     $(block).addClass ('.present');
+    // }
+}
+
+
+
+// save description
+var saveBlock = function(){
+    var text = localStorage.getItem('textarea');
+   
+
+
+};
+
+saveBtn.addEventListener("click", function ( ) {
+
+    var text = document.querySelector('textarea').value;
+
+    localStorage.setItem('text', text);
+    saveBlock();
+
+})
+
+
+
+
+
+
 
