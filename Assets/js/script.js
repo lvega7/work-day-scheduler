b315e43746514a03b9162dd2cc6000ace70186c5
Lissette Vega <lissettevega@Lissettes-MacBook-Pro.local>
2021-09-25 18:39:37 -0400

    update local storage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,7 @@
 var date = document.querySelector("#currentDay")
 var block = document.querySelector("textarea")
-var saveBtn= document.querySelector(".saveBtn")
+var time = document.querySelector("time")
+var saveBtn= document.querySelectorAll(".saveBtn")
 
 // Current date displayed on top
 var day = moment ().format('dddd, MMMM Do');
@@ -12,30 +13,30 @@ date.textContent= (day)
 
 
 var colorTime = function(){
-var now = new Date();
-var hours = now.getHours();
-
-var ft = now.toLocaleString("en-US", {
-    hour: "numeric",
-    minute: "numeric",
-    hour12: true
-});
-
-if (5 <= hours && hours < 8) {//Morning
-    $(block).addClass ('.present');
-    
-}}
+    if(time > moment ()){
+        block.addClass=("past")
+    }
+}
 
 
 
 
 // save block text 
-block.value=localStorage.getItem("notes")
 
 
-saveBtn.addEventListener("click", event=> {
-    localStorage.setItem("notes", block.value)
-})
+document.getElementById("hour-09").value = localStorage.getItem("hour-09")
+document.getElementById("hour-10").value = localStorage.getItem("hour-10")
+
+for (i=0; i<saveBtn.length; i++) {
+    
+    saveBtn[i].addEventListener("click", event=> {
+        localStorage.setItem(event.target.previousElementSibling.id,event.target.previousElementSibling.value)
+        console.log(block.value)
+        console.log(event.target.previousElementSibling.id)
+    })
+}
+
+
 
 
 
