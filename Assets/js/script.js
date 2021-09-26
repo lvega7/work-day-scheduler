dcf5ac544758bd4f2486a5efe71a2b80bc2e09d6
Lissette Vega <lissettevega@Lissettes-MacBook-Pro.local>
2021-09-26 18:58:22 -0400

    update readme

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,6 +2,8 @@ var date = document.querySelector("#currentDay")
 var block = document.querySelector("textarea")
 var time = document.querySelector("time")
 var saveBtn= document.querySelectorAll(".saveBtn")
+var hour09 = document.getElementById("hour-09")
+var rows = document.querySelector("time")
 
 // Current date displayed on top
 var day = moment ().format('dddd, MMMM Do');
@@ -13,10 +15,21 @@ date.textContent= (day)
 
 
 var colorTime = function(){
-    if(time > moment ()){
-        block.addClass=("past")
+    if( timeNine === day.dddd){
+        console.log("good")
+    }
+    else{
+        console.log(nonono)
     }
 }
+
+
+
+
+
+
+
+
 
 
 
@@ -26,6 +39,13 @@ var colorTime = function(){
 
 document.getElementById("hour-09").value = localStorage.getItem("hour-09")
 document.getElementById("hour-10").value = localStorage.getItem("hour-10")
+document.getElementById("hour-11").value = localStorage.getItem("hour-11")
+document.getElementById("hour-12").value = localStorage.getItem("hour-12")
+document.getElementById("hour-1").value = localStorage.getItem("hour-1")
+document.getElementById("hour-2").value = localStorage.getItem("hour-2")
+document.getElementById("hour-3").value = localStorage.getItem("hour-3")
+document.getElementById("hour-4").value = localStorage.getItem("hour-4")
+document.getElementById("hour-5").value = localStorage.getItem("hour-5")
 
 for (i=0; i<saveBtn.length; i++) {
     
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # work-day-scheduler
+
+For this project I created a work day scheduler. This website can let you write in any task throughout the work day and save it. The colors of the tasks changes depending on the time and if the task time has passed or night.
+
+Link to page: https://lvega7.github.io/work-day-scheduler/
+
